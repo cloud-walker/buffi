ae273598852f0d674351a7e46e7ff946a537f8cf
Luca Barone <baro.luc@gmail.com>
2026-07-07 10:13:56 +0200

    add panda CSS

---
+++ apps/app/src/App.tsx
@@ -0,0 +1,13 @@
+import { css } from "../styled-system/css";
+
+export function App() {
+  return (
+    <div
+      className={css({
+        textStyle: "2xl",
+      })}
+    >
+      Buffi!!
+    </div>
+  );
+}
--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,9 +1,12 @@
 import { createRoot } from "react-dom/client";
 
+import "./main.css";
+import { App } from "./App";
+
 const rootElement = document.getElementById("root");
 
 if (rootElement == null) {
   throw new Error("#root element not found.");
 }
 
-createRoot(rootElement).render(<>BUFFI!!!</>);
+createRoot(rootElement).render(<App />);
